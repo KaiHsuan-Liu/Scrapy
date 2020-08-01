99d5bc7451ed9eb2fef5ef4a25ca8f0b47cd13e1
Kai Liu <love83030414@gmail.com>
2020-08-02 01:16:32 +0800

    Scrapy and Unittest.
    Signed-off-by: Kai Liu <love83030414@gmail.com>

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+import unittest
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('http://www.pythonscraping.com/pages/page1.html')
-bs = BeautifulSoup(html.read(), 'html.parser')
-print(bs.h1)
+class testTitle(unittest.TestCase):
+    def setUpClass():
+        print('setUp~~')
+        global bsObj
+        url = 'http://en.wikipedia.org/wiki/Monty_Python'
+        bsObj = BeautifulSoup(urlopen(url))
+
+    # def tearDown(self):
+        # print('tearDown~~~')
+
+    def test_add(self):
+        total = 2+2
+        self.assertEqual(total, 4)
+
+    def test_title(self):
+        pageTitle = bsObj.find('h1').get_text()
+        self.assertEqual('Monty Python', pageTitle)
+
+if __name__ == "__main__":
+    unittest.main()
